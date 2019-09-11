b124c53d719b7e10a082941bbbabc49eb2d2caae
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-11 11:35:49 +0200

    fix(index): move hygen helpers over to index file

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 const { runner } = require('hygen')
 const Logger = require('hygen/lib/logger')
-const path = require('path')
-const fs = require('fs')
 const inquirer = require('inquirer');
 const QUESTIONS = require('./constants/questions')
 const defaultTemplates = `${__dirname}/_templates`
+const { pascalCaseIt, kebabCaseIt } = require('case-it')
 
 inquirer.prompt(QUESTIONS)
   .then(answers => {
@@ -18,6 +17,26 @@ inquirer.prompt(QUESTIONS)
       cwd: process.cwd(),
       logger: new Logger(console.log.bind(console)),
       createPrompter: () => require('enquirer'),
+      helpers: {
+        pascalName: name => {
+          const packageName = stripOrg(name)
+    
+          if (packageName) {
+            return pascalCaseIt(packageName)
+          } else {
+            return ''
+          }
+        },
+        kebabName: name => {
+          const packageName = stripOrg(name)
+    
+          if (packageName) {
+            return kebabCaseIt(packageName)
+          } else {
+            return ''
+          }
+        }
+      },
       exec: (action, body) => {
         const opts = body && body.length > 0 ? { input: body } : {}
         return require('execa').shell(action, opts)
@@ -25,3 +44,8 @@ inquirer.prompt(QUESTIONS)
       debug: !!process.env.DEBUG
     })
   });
+
+function stripOrg (name) {
+  return name.split('/').filter(part => !part.includes('@')).join('')
+}
+  
